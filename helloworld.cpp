303d15fb8a87114d9f0f21ef829bf2f7ab499293
EspinosaJV <152014605+EspinosaJV@users.noreply.github.com>
2025-09-25 18:55:07 +0800

    Lesson 9 C++

--- helloworld.cpp
+++ helloworld.cpp
@@ -1,21 +1,23 @@
 #include <iostream>
-
-// cout << (insertion operator)
-// cin >> (extraction operator)
+#include <cmath>
 
 int main()
 {
-    std::string name;
-    int age;
+    double x = 3;
+    double y = 4;
+    double z;
 
-    std::cout << "Whats your age?: ";
-    std::cin >> age;
+    //z = std::max(x, y);
+    //z = std::min(x,y);
+    //z = pow(2, 4);
+    //z = sqrt(9);
+    //z = abs(-3);
+    //z = round(x);
+    //z = ceil(x);
+    z = floor(x);
 
-    std::cout << "What's your full name?: ";
-    std::getline(std::cin >> std::ws, name);
+    std::cout << z;
 
-    std::cout << "Hello " << name << '\n';
-    std::cout << "You are " << age << " years old";
 
     return 0;
 }
