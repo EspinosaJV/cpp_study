98f67ebcbfc5f6a051cc423e27d0dac1a2712693
EspinosaJV <152014605+EspinosaJV@users.noreply.github.com>
2025-09-29 19:30:11 +0800

    Lesson 17 C++

--- helloworld.cpp
+++ helloworld.cpp
@@ -2,34 +2,14 @@
 
 int main()
 {
-    double temp;
-    char unit;
+    std::string name;
 
-    std::cout << "******** Temperature conversion ********\n";
+    std::cout << "Enter your name: ";
+    std::getline(std::cin, name);
 
-    std::cout << "F = Fahrenheit\n";
-    std::cout << "C = Celsius\n";
-    std::cout << "What unit would you like to convert to: ";
-    std::cin >> unit;
+    name.erase(0, 3);
 
-    if(unit == 'F' || unit == 'f'){
-        std::cout << "Enter the temperature in Celsius: ";
-        std::cin >> temp;
-
-        temp = (1.8 * temp) + 32.0;
-        std::cout << "Temperature is: " << temp << "F\n";
-    }
-    else if(unit == 'C' || unit == 'c'){
-        std::cout << "Enter the temperature in Fahrenheit: ";
-        std::cin >> temp;
-
-        temp = (temp - 32) / 1.8;
-        std::cout << "Temperature is: " << temp << "C\n";
-    }
-    else{
-    }
-
-    std::cout << "****************************************\n";
+    std::cout << name;
 
     return 0;
 }
