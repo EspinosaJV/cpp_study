9182ac3e669eef3d67d071447745fa0b15562d7c
EspinosaJV <152014605+EspinosaJV@users.noreply.github.com>
2025-09-29 20:55:47 +0800

    Lesson 21 C++

--- helloworld.cpp
+++ helloworld.cpp
@@ -2,11 +2,13 @@
 
 int main()
 {
-    for(int i = 10; i >= 0; i-=2){
+    // break = break out of a loop
+    // continue = skip current iteration
+
+    for(int i = 1; i <= 20; i++){
+        if(i == 13){
+            continue;
+        }
         std::cout << i << '\n';
     }
-    
-    std::cout << "HAPPY NEW YEAR!\n";
-
-    return 0;
 }
