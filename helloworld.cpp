a69f2aeb8280d9c5afc311a4638e5f1a9917e311
EspinosaJV <152014605+EspinosaJV@users.noreply.github.com>
2025-10-06 18:14:33 +0800

    Lesson 30 C++

--- helloworld.cpp
+++ helloworld.cpp
@@ -1,24 +1,83 @@
 #include <iostream>
+#include <iomanip>
 
-void bakePizza();
-void bakePizza(std::string topping1);
-void bakePizza(std::string topping1, std::string topping2);
+void showBalance(double balance);
+double deposit();
+double withdraw(double balance);
 
 int main()
 {
-    bakePizza("pepperoni", "mushrooms");
+    double balance = 0;
+    int choice = 0;
+
+    do{
+        std::cout << "******************\n";
+        std::cout << "Enter your choice:\n";
+        std::cout << "******************\n";
+        std::cout << "1. Show Balance\n";
+        std::cout << "2. Deposit Money\n";
+        std::cout << "3. Withdraw Money\n";
+        std::cout << "4. Exit\n";
+        std::cin >> choice;
+
+        std::cin.clear();
+        fflush(stdin);
+
+        switch(choice){
+            case 1: showBalance(balance);
+                    break;
+            case 2: balance += deposit();
+                    showBalance(balance);
+                    break;
+            case 3: balance -= withdraw(balance);
+                    showBalance(balance);
+                    break;
+            case 4: std::cout << "Thanks for visiting!\n";
+                    break;
+            default:std::cout << "Invalid choice\n";
+                    break;
+        }
+    }while(choice != 4);
 
     return 0;
 }
 
-void bakePizza(){
-    std::cout << "Here is your pizza!\n";
+void showBalance(double balance){
+    std::cout << "Your balance is: $" << std::setprecision(2) << std::fixed << balance << '\n';
 }
+double deposit(){
 
-void bakePizza(std::string topping1){
-    std::cout << "Here is your " << topping1 << " pizza!\n";
+    double amount = 0;
+
+    std::cout << "Enter amount to be deposited: ";
+    std::cin >> amount;
+
+    if(amount > 0){
+        return amount;
+    }
+    else{
+        std::cout << "That's not a valid amount:\n";
+        return 0;
+    }
+
+    return amount;
 }
+double withdraw(double balance){
 
-void bakePizza(std::string topping1, std::string topping2){
-    std::cout << "Here is your " << topping1 << " and " << topping2 << " pizza!\n";
+    double amount = 0;
+
+    std::cout << "Enter amount to be withdrawn: ";
+    std::cin >> amount;
+
+    if(amount > balance){
+        std::cout << "Insufficient funds\n";
+        return 0;
+    }
+    else if (amount < 0){
+        std::cout << "That's not a valid amount\n";
+        return 0;
+    }
+    else{
+        return amount;
+    }
 }
