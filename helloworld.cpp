29dab31aafe4026091c462f896b3148820d7404a
EspinosaJV <152014605+EspinosaJV@users.noreply.github.com>
2025-10-15 19:22:01 +0800

    Lesson 40 C++

--- helloworld.cpp
+++ helloworld.cpp
@@ -2,18 +2,25 @@
 
 int main()
 {
-    // fill() = Fills a range of elements with a specified value
-    //          fill(begin, end, value)
+    std::string foods[5];
+    int size = sizeof(foods)/sizeof(foods[0]);
+    std::string temp;
 
-    const int SIZE = 99;
-    std::string foods[SIZE];
+    for(int i = 0; i < size; i++){
+        std::cout << "Enter a food you like or 'q' to quit #" << i + 1 << ": ";
+        std::getline(std::cin, temp);
+        if(temp == "q"){
+            break;
+        }
+        else{
+            foods[i] = temp;
+        }
+    }
 
-    fill(foods, foods + (SIZE/3), "pizza");
-    fill(foods + (SIZE/3), foods + (SIZE/3)*2, "hamburger");
-    fill(foods + (SIZE/3)*2, foods + SIZE, "hotdog");
+    std::cout << "You like the following food:\n";
 
-    for(std::string food : foods){
-        std::cout << food << '\n';
+    for(int i = 0; !foods[i].empty(); i++){
+        std::cout << foods[i] << '\n';
     }
 
     return 0;
