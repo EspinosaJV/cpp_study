0e7df4d4c3e32ab8117c48c7e2dbbf686260b331
EspinosaJV <152014605+EspinosaJV@users.noreply.github.com>
2025-10-30 02:20:51 +0800

    Lesson 52 C++

--- helloworld.cpp
+++ helloworld.cpp
@@ -1,23 +1,11 @@
 #include <iostream>
-
-int factorial(int num);
-
-int main () {
-
-    // recursion = a programming technique where a function
-    //             invokes itself from within
-    //             break a complex concept into a repeatable single step
-
-    std::cout << factorial(10);
+template <typename T, typename U>
+auto max(T x, U y){
+    return (x > y) ? x : y;
+}
+int main()
+{
+    std::cout << max(1, 2.1) << '\n';
 
     return 0;
-}
-
-int factorial(int num){
-    if(num > 1){
-        return num * factorial(num - 1);
-    }
-    else{
-        return 1;
-    }
 }
