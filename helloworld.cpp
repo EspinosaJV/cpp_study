978ead5f0e0237eda7b88131f169ba59139ce1ae
EspinosaJV <152014605+EspinosaJV@users.noreply.github.com>
2025-11-07 16:20:52 +0800

    Lesson 54 C++

--- helloworld.cpp
+++ helloworld.cpp
@@ -1,41 +1,41 @@
 #include <iostream>
 
-struct student{
-    std::string name;
-    double gpa;
-    bool enrolled = true;
+struct Car{
+    std::string model;
+    int year;
+    std::string color;
 };
 
-int main()
-{
-    // struct = A structure that group related variables under one name
-    //          structs can contain many different data types (string, int, double bool, etc.)
-    //          variables in a struct are known as "members"
-    //          members can be access with . "Class Member Access Operators"
+void printCar(Car &car);
+void paintCar(Car &car, std::string color);
 
-    student student1;
-    student1.name = "Spongebob";
-    student1.gpa = 3.2;
+int main () {
 
-    student student2;
-    student2.name = "Patrick";
-    student2.gpa = 2.1;
+    Car car1;
+    Car car2;
 
-    student student3;
-    student3.name = "Squidward";
-    student3.gpa = 1.5;
+    car1.model = "Mustang";
+    car1.year = 2023;
+    car1.color = "red";
 
-    std::cout << student1.name << '\n';
-    std::cout << student1.gpa << '\n';
-    std::cout << student1.enrolled << '\n';
+    car2.model = "Corvette";
+    car2.year = 2024;
+    car2.color = "blue";
 
-    std::cout << student2.name << '\n';
-    std::cout << student2.gpa << '\n';
-    std::cout << student2.enrolled << '\n';
+    paintCar(car1, "silver");
+    paintCar(car2, "gold");
+    
+    printCar(car1);
+    printCar(car2);
 
-    std::cout << student3.name << '\n';
-    std::cout << student3.gpa << '\n';
-    std::cout << student3.enrolled << '\n';
-     
     return 0;
+}
+void printCar(Car &car){
+    std::cout << &car << '\n';
+    std::cout << car.model << '\n';
+    std::cout << car.year << '\n';
+    std::cout << car.color << '\n';
+}
+void paintCar(Car &car, std::string color){
+    car.color = color;
 }
