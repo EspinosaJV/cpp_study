7388cab51ae8a5717f7af5b1f036b4657face6e1
EspinosaJV <152014605+EspinosaJV@users.noreply.github.com>
2025-11-07 16:30:07 +0800

    Lesson 55 C++

--- helloworld.cpp
+++ helloworld.cpp
@@ -1,41 +1,37 @@
 #include <iostream>
 
-struct Car{
-    std::string model;
-    int year;
-    std::string color;
-};
+enum Day {sunday = 0, monday = 1, tuesday = 2, wednesday = 3, thursday = 4, friday = 5, saturday = 6};
 
-void printCar(Car &car);
-void paintCar(Car &car, std::string color);
+enum Flavor {vanilla, chocolate, strawberry, mint};
+
+enum Color {red, orange, yellow, green, blue, purple};
+
+enum Planet {mercury = 4880, venus = 12104, earth = 12756, mars = 6794, jupiter = 142984, saturn = 108728, uranus = 51118, neptune = 49532, pluto = 2320};
 
 int main () {
 
-    Car car1;
-    Car car2;
+    // enums = a user-defined data type that consists
+    //         of paired named-integer constants.
+    //         GREAT if you have a set of potential options
 
-    car1.model = "Mustang";
-    car1.year = 2023;
-    car1.color = "red";
+    Day today = friday;
 
-    car2.model = "Corvette";
-    car2.year = 2024;
-    car2.color = "blue";
-
-    paintCar(car1, "silver");
-    paintCar(car2, "gold");
-    
-    printCar(car1);
-    printCar(car2);
+    switch(today){
+        case sunday:  std::cout << "It is Sunday!\n";
+                        break;
+        case monday:  std::cout << "It is Monday!\n";
+                        break;
+        case tuesday: std::cout << "It is Tuesday!\n";
+                        break;
+        case wednesday:   std::cout << "It is Wednesday!\n";
+                            break;
+        case thursday:    std::cout << "It is Thursday!\n";
+                            break;
+        case friday:  std::cout << "It is Friday!\n";
+                        break;
+        case saturday:    std::cout << "It is Saturday!\n";
+                            break;
+    }
 
     return 0;
-}
-void printCar(Car &car){
-    std::cout << &car << '\n';
-    std::cout << car.model << '\n';
-    std::cout << car.year << '\n';
-    std::cout << car.color << '\n';
-}
-void paintCar(Car &car, std::string color){
-    car.color = color;
 }
