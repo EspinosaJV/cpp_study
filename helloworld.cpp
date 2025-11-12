0773675e2a1a58bd9bb1bb5a9bd5785832160fc0
EspinosaJV <152014605+EspinosaJV@users.noreply.github.com>
2025-11-12 15:09:37 +0800

    Lesson 58 C++

--- helloworld.cpp
+++ helloworld.cpp
@@ -1,37 +1,28 @@
 #include <iostream>
 
-class Car{
+class Pizza{
     public:
-        std::string make;
-        std::string model;
-        int year;
-        std::string color;
+        std::string topping1;
+        std::string topping2;
 
-    Car(std::string make, std::string model, int year, std::string color){
-        this->make = make;
-        this->model = model;
-        this->year = year;
-        this->color = color;
-    }
+    
+        Pizza(){
+
+        }
+        Pizza(std::string topping1){
+            this->topping1 = topping1;
+        }
+        Pizza(std::string topping1, std::string topping2){
+            this->topping1 = topping1;
+            this->topping2 = topping2;
+        }
 };
 
 int main() {
 
-    // constructor = special method that is automatically called when 
-    //               useful for assigning values to attributes as arguments
-
-    Car car1("Chevy", "Corvette", 2022, "blue");
-    Car car2("Ford", "Mustang", 2023, "red");
-
-    std::cout << car1.make << '\n';
-    std::cout << car1.model << '\n';
-    std::cout << car1.year << '\n';
-    std::cout << car1.color << '\n';
-
-    std::cout << car2.make << '\n';
-    std::cout << car2.model << '\n';
-    std::cout << car2.year << '\n';
-    std::cout << car2.color << '\n';
+    Pizza pizza1("pepperoni");
+    Pizza pizza2("mushrooms", "peppers");
+    Pizza pizza3;
 
     return 0;
 }
