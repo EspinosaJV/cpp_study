681d78ee8e5e69d319c8c4b0ec4ff4db32cf9e6f
EspinosaJV <152014605+EspinosaJV@users.noreply.github.com>
2025-11-17 21:25:21 +0800

    Arithmetic Operators in C++

--- helloworld.cpp
+++ helloworld.cpp
@@ -1,38 +1,39 @@
 #include <iostream>
 
-class Shape{
-    public:
-        double area;
-        double volume;
-};
-class Cube : public Shape{
-    public:
-        double side;
-    Cube(double side){
-        this->side = side;
-        this->area = side * side * 6;
-        this->volume = side * side * side;
-    }
-};
-class Sphere : public Shape{
-    public:
-        double radius;
-    Sphere(double radius){
-        this->radius = radius;
-        this->area = 4 * 3.14159 * (radius * radius);
-        this->volume = (4/3.0) * 3.14159 * (radius * radius * radius);
-    }
-};
-int main() {
+int main(){
+    //1. Addition Operation (+)
+    //It adds two numbers together
+    int a = 1;
+    int b = 2;
+    int sum = a + b;
 
-    Cube cube(10);
-    Sphere sphere(5);
+    //2. Subtraction Operator (-)
+    //It subtracts one number from another.
+    int difference = a - b;
 
-    std::cout << "Area: " << cube.area << "cm\n";
-    std::cout << "Volume: " << cube.volume << "cm\n";
+    //3. Multiplication Operator (*)
+    //It multiplies two numbers together.
+    int product = a * b;
 
-    std::cout << "Area: " << sphere.area << "cm\n";
-    std::cout << "Volume: " << sphere.volume << "cm\n";
+    //4. Division Operator(/)
+    //it divides one number by another. Note that IF BOTH OPERANDS ARE INTEGERS, IT WILL PERFFORM INTEGER DIVISION AND THE RESULT WILL BE AN INTEGER.
+    int quotient = a / b; // integer division
+    float quotient = float(a) / float(b) // floating-point division
 
-    return 0;
+    //5. Modulos Operator(%)
+    //It calculates the remainder of an integer division.
+    int remainder = a % b;
+
+    //6.Increment Operator (++)
+    //It increments the value of a variable by 1. There are two ways to use this operator, prefix (++x), and postfix (x++)
+    //Prefix increments the value before returning it whereas postfix returns the value first AND THEN increments it
+    int x = 5;
+    int y = ++x; // x = 6 y = 6
+    int z = x++; // x = 7 z = 6
+
+    //7. Decrement Operator(--)
+    //It decrements the value of a variable by 1. It can also BE USED IN PREFIX (--X) and postfix(x--) formsz
+    int x = 5;
+    int y = --x;
+    int z = x--;
 }
