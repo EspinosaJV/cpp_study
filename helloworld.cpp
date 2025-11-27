7ac59506f173a5b92d2937b2753e6a0694be4769
EspinosaJV <152014605+EspinosaJV@users.noreply.github.com>
2025-11-27 18:56:34 +0800

    Bitwise Operations

--- helloworld.cpp
+++ helloworld.cpp
@@ -1,26 +1,12 @@
-#include<iostream>
+#include <iostream>
 
-int main(){
-    // int a = 5, b = 10;
-    // if (a > 0 && b > 0){
-    //     std::cout << "Both values are positive.\n";
-    // }
-
-    // int a = 5, b = -10;
-    // if (a > 0 || b > 0){
-    //     std::cout << "At least one value is positive.\n";
-    // }
-
-    // int a = 5;
-    // if (!(a < 0)){
-    //     std::cout << "The value is not negative.\n";
-    // }
-
-    int a = 5, b = -10, c = 15;
-
-    if (a > 0 && (b > 0 || c > 0)) {
-        std::cout << "At least two values are positive.\n";
-    }
-
-    return 0;
+int main() {
+    // int result = 5 & 3;
+    // int result = 5 | 3;
+    // int result = 5 ^ 3;
+    //int result = ~5;
+    //int result = 5 << 1;
+    //int result = 5 >> 1;
+    
+    std::cout << result;
 }
