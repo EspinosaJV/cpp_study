259155c40552583935db2eab7aacfee1b709334d
EspinosaJV <152014605+EspinosaJV@users.noreply.github.com>
2025-12-01 18:45:40 +0800

    Functions in C++

--- helloworld.cpp
+++ helloworld.cpp
@@ -1,18 +1,11 @@
 #include <iostream>
 
-int main() {
-    int weeks = 3;
-    int daysInWeek = 7;
+void foo(){ // no longer inside of main()
+    std::cout << "foo!\n";
+}
 
-    for (int i = 1; i <= weeks; i++){
-        std::cout << "Week: " << i << std::endl;
+int main(){
+    foo();
 
-        for (int j = 1; j <= daysInWeek; j++){
-            // continue if the day is an odd number
-            if (j % 2 != 0) {
-                continue;
-            }
-            std::cout << "Day: " << j << std::endl;
-        }
-    }
+    return 0;
 }
