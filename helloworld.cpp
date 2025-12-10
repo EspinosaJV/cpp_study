180ef94fd22b04f19ca287987674775400588ae0
EspinosaJV <152014605+EspinosaJV@users.noreply.github.com>
2025-12-10 10:31:16 +0800

    Operator Overloading in C++

--- helloworld.cpp
+++ helloworld.cpp
@@ -1,11 +1,43 @@
 #include <iostream>
+using namespace std;
 
-void foo(){ // no longer inside of main()
-    std::cout << "foo!\n";
-}
+class Complex {
+    private:
+        int real, imag;
+    public:
+        Complex(){
+            real = 0;
+            imag = 0;
+        }
+
+        Complex(int r, int i){
+            real = r;
+            imag = i;
+        }
+
+        void print(){
+            cout << real << " + " << imag << "i" << endl;
+        }
+
+        // Operator overloading syntax
+        Complex operator +(Complex c){
+            Complex temp;
+            temp.real = real + c.real;
+            temp.imag = imag + c.imag;
+            return temp;
+        }
+};
 
 int main(){
-    foo();
 
+    Complex c1(5,4);
+    Complex c2(2,5);
+    Complex c3(1,1);
+    Complex c4;
+
+    c4 = c1 + c2;
+
+    c4.print();
+    
     return 0;
 }
