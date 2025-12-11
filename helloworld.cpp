cc10b08a70758dea0a240c9653818a4e373900ce
EspinosaJV <152014605+EspinosaJV@users.noreply.github.com>
2025-12-11 17:00:45 +0800

    Static Polymorphism Part 1

--- helloworld.cpp
+++ helloworld.cpp
@@ -1,24 +1,15 @@
 #include <iostream>
-#include <vector>
-#include <algorithm>
-using namespace std;
+
+// Template function to print ANY type
+template<typenameT>
+void print(const T& value){
+    std::cout << "Printing value: " << value << "\n";
+}
 
 int main(){
-    int d = 7;
-    int e = 5;
-    std::vector <int> v{2, 3, 7, 14, 23};
-    std::for_each(v.begin(), v.end(), [&](int x){
-        if(x % d == 0){
-            std::cout << x << " is divisible by " << d << "\n";
-        } else {
-            std::cout << x << " is not divisible by " << d << "\n";
-        }
-        d = 10;
-        e = 19;
-        std::cout << d << " is the new d value" << "\n";
-        std::cout << e << " is the new e value" << "\n";
-    });
+    print(42);
+    print(3.14159);
+    print("Hello");
 
-    system("pause>nul");
     return 0;
 }
