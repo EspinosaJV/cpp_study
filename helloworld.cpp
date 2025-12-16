4d17235f77b71a8ec0881fe74537aa1e7238d0e3
EspinosaJV <152014605+EspinosaJV@users.noreply.github.com>
2025-12-16 19:26:13 +0800

    Function Overloading in C++

--- helloworld.cpp
+++ helloworld.cpp
@@ -1,35 +1,25 @@
-// TOPIC: Polymorphism in C++!! (Very Important Feature of OOP)
-
-// POINTS:
-// There are two types of polymorphism
-// A. Compile Time Polymorphism / Static Binding / Early Binding
-//      a. Function Overloading
-//      b. Operator Overloading
-// B. Runtime Polymorphism / Dynamic Binding / Lazy Binding
-//      a. Function Overriding (Using virtual functions)
-
 #include <iostream>
-using namespace std;
 
-class Base{
-    public:
-        virtual void fun(int x){
-            cout << "Base" << endl;
-        }
-};
-
-class Derived : public Base{
-    public:
-        virtual void fun(int x) override{
-            cout << "Derived" << endl;
-        }
-};
+void bakePizza();
+void bakePizza(std::string topping1);
+void bakePizza(std::string topping1, std::string topping2);
 
 int main(){
-    Derived d;
-    Base &b = d;
+    bakePizza();
+    bakePizza("pepperoni");
+    bakePizza("pepperoni", "mushrooms");
     
-    b.fun(10);
+    return 0;
+}
 
-    return 0; 
+void bakePizza(){
+    std::cout << "Here is your pizza!" << std::endl;
+}
+
+void bakePizza(std::string topping1){
+    std::cout << "Here is your " << topping1 << " pizza!\n";
+}
+
+void bakePizza(std::string topping1, std::string topping2){
+    std::cout << "Here is your " << topping1 << " and " << topping2 << " pizza!\n";
 }
