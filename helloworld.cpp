c43f7be6b04e0e89ac8bb021897e272da684c174
EspinosaJV <152014605+EspinosaJV@users.noreply.github.com>
2025-12-17 13:22:39 +0800

    C++ Function Overloading Part 3

--- helloworld.cpp
+++ helloworld.cpp
@@ -1,27 +1,37 @@
 #include <iostream>
 using namespace std;
 
-int sum(int a, int b);
-double sum(double a, double b);
-float sum(float a, float b, float c);
+int add(int a, int b){ // function 1
+    cout << "This is function 1!" << endl;
+    return a + b;
+}
+
+double add(double a, double b){ // function 2
+    cout << "This is function 2!" << endl;
+    return a + b;
+}
+
+int add(int a, int b, int c){ // function 3
+    cout << "This is function 3!" << endl;
+    return a + b + c;
+}
+
+double add(int a, double b){ // function 4
+    cout << "This is function 4!" << endl;
+    return a + b;
+}
+
+double add(double a, int b){ // function 5
+    cout << "This is function 5!" << endl;
+    return a + b;
+}
 
 int main(){
+    cout << "addition is: " << add(1,2) << endl; // function 1
+    cout << "addition is: " << add(3.5, 4.5) << endl; // function 2
+    cout << "addition is: " << add(3, 4, 5) << endl; // function 3
+    cout << "addition is: " << add(3, 4.5) << endl; // function 4
+    cout << "addition is: " << add(3.5, 4) << endl; // function 5
 
-    cout << sum(4, 3) << endl;
-    cout << sum(4.4, 3.3) << endl;
-    cout << sum(4.4, 3.3, 2.2) << endl;
-    
     return 0;
-}
-
-int sum(int a, int b){
-    return a + b;
-}
-
-double sum(double a, double b){
-    return a + b;
-}
-
-float sum(float a, float b, float c){
-    return a + b + c;
 }
